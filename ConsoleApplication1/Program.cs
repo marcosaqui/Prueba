c9064b5995674c393bfbdc068c82825149ac4a7c
marcosaqui <marcos123400@gmail.com>
2019-08-23 20:32:01 -0400

    mkomjojm

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Segundo Cambio");
-            Console.WriteLine("Tercer Cambio");
+            Console.WriteLine("Cuartoio");
+            
         }
     }
 }
