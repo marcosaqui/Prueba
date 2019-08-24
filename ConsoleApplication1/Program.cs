3f9323638e4b82e468e025bf951fab6204b7dabb
marcosaqui <marcos123400@gmail.com>
2019-08-23 20:14:02 -0400

    Impresion normal

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Primer Cambio");
             Console.WriteLine("Segundo Cambio");
+            Console.WriteLine("Tercer Cambio");
         }
     }
 }
